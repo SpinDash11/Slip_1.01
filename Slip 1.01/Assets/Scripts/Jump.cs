f92f6bd090651338d8947d09dc2f22ccc295e5a3
Logan Valdez <loganvaldez04@gmail.com>
2024-04-01 13:38:08 -0700

    Fixed Main Level

--- Slip 1.01/Assets/Scripts/Jump.cs
+++ Slip 1.01/Assets/Scripts/Jump.cs
@@ -14,13 +14,6 @@ public class Jump : MonoBehaviour
         {
             rb.AddForce(Vector3.up * 200);
             isJumping = true;
-            rb.GetComponent<Rigidbody>();
-            rb.mass = 2;
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift)) 
-        {
-            rb.AddForce(-Vector3.up * 5000);
         }
     }
 
@@ -29,13 +22,11 @@ public class Jump : MonoBehaviour
         if ( collision.gameObject.CompareTag("Ground"))
         {
             isJumping = false;
-            rb.drag = 0;
         }
 
         if (collision.gameObject.CompareTag("Wall"))
         {
             isJumping = false;
-            rb.mass = 2;
         }
     }
 }
