66a3bac43e9d5c316fb42b3b69efaf7e241b3734
grafael148 <81830869+grafael148@users.noreply.github.com>
2024-04-22 17:14:25 -0700

    Reapply "worked on levels"
    This reverts commit 8861187a16653a0345717499f3838949a222b222.

---
+++ Slip 1.01/Assets/Scripts/Level_1_PlayerGoal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_1_PlayerGoal : MonoBehaviour
+{
+
+    public GameObject playerGoal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerGoal.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            playerGoal.SetActive(false);
+        }
+    }
+
+}
